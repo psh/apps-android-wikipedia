9aa47a32e10b22d90a9ee7b2f6908442cbad428d
Paul Hawke <paul.hawke@gmail.com>
2022-11-01 10:53:23 -0500

    KMM SPM package release for 0.8.6

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let remoteKotlinUrl = "https://gitlab.com/api/v4/projects/40523916/packages/maven/apps-android-wikipedia/kotlinmultiplatformsharedmodule-kmmbridge/0.8.5/kotlinmultiplatformsharedmodule-kmmbridge-0.8.5.zip"
-let remoteKotlinChecksum = "76b4542545ecb2438f3fda615712bb230ff7c58b5d80b63e42f92f881d27c7f2"
+let remoteKotlinUrl = "https://gitlab.com/api/v4/projects/40523916/packages/maven/apps-android-wikipedia/kotlinmultiplatformsharedmodule-kmmbridge/0.8.6/kotlinmultiplatformsharedmodule-kmmbridge-0.8.6.zip"
+let remoteKotlinChecksum = "1b501a9920bdb1827625ef20e9307126bdc852c904f17efc266c1b9dbbc643a8"
 let packageName = "kotlinmultiplatformsharedmodule"
 
 let package = Package(
