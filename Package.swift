f95d3b507e53ce9fd688233d9af5e9e21c6e160a
Paul Hawke <paul.hawke@gmail.com>
2022-11-01 10:51:05 -0500

    Cleanup

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let remoteKotlinUrl = "https://gitlab.com/api/v4/projects/40523916/packages/maven/apps-android-wikipedia/kotlinmultiplatformsharedmodule-kmmbridge/0.8.4/kotlinmultiplatformsharedmodule-kmmbridge-0.8.4.zip"
-let remoteKotlinChecksum = "fa9392805ad2e12f7e36dbc6b61f00971e00b642583ac89bb109ed0e1960680f"
+let remoteKotlinUrl = "https://gitlab.com/api/v4/projects/40523916/packages/maven/apps-android-wikipedia/kotlinmultiplatformsharedmodule-kmmbridge/0.8.5/kotlinmultiplatformsharedmodule-kmmbridge-0.8.5.zip"
+let remoteKotlinChecksum = "76b4542545ecb2438f3fda615712bb230ff7c58b5d80b63e42f92f881d27c7f2"
 let packageName = "kotlinmultiplatformsharedmodule"
 
 let package = Package(
